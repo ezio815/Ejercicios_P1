33a46aaf446bdfbc28a6516de0f8c134a0df9c31
Ezi <exio78@hotmail.com>
2021-08-31 18:10:57 -0300

    6 / 4

--- 6/Ej4.pas
+++ 6/Ej4.pas
@@ -6,7 +6,7 @@ begin
     Write('Ingrese la cantidad de numeros primos: ');
     ReadLn(n);
     primos := 0;
-    numero := 2;
+    numero := 1;
     WriteLn;
     Write('Las raices cuadradas de los primeros ', n:0, ' numeros primos son:');
     for i := 1 to n do
@@ -14,13 +14,12 @@ begin
         while primos < i do
         begin
             primo := true;
+            numero := numero + 1;
             for j := numero -1 downto 2 do
                 if numero mod j = 0 then
                     primo := false;
             if primo then
                 primos := primos + 1
-            else
-                numero := numero + 1
         end;
         Write(' ', sqrt(numero):0:2)
     end
